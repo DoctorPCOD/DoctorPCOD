0ce87a4c0b2aed2d9cea431804ab00263057f648
DoctorPCOD <78432183+DoctorPCOD@users.noreply.github.com>
2021-11-17 14:27:02 -0600

    Add files via upload

---
+++ ENGIN-2223/lab2_circuit1.ino
@@ -0,0 +1,51 @@
+/*
+  Lab 2, Circuit 1
+  Using a pushbutton to turn on/off an LED
+  By: Alyssa J. Pasquale, Ph.D.
+  Written: May 15, 2017
+  Edited: May 1, 2019
+  I/O Pins
+  A0:
+  A1:
+  A2:
+  A3:
+  A4:
+  A5:
+  D0:
+  D1:
+  D2:
+  D3:
+  D4:
+  D5:
+  D6:
+  D7:
+  D8:  Pushbutton
+  D9:
+  D10:
+  D11:
+  D12:
+  D13: LED
+*/
+
+void setup()
+{
+  // Set DDRB bit 5
+  // This causes Arduino pin D13 to be an output
+  DDRB = 0x20;
+}
+
+void loop()
+{
+  //Read value from pushbutton (D8)
+  unsigned char buttonState = PINB & 0x01;
+
+  if (buttonState) {
+    //Turn on LED
+    PORTB = PORTB | 0x20;
+  }
+  else {
+    // Turn off LED
+    PORTB = PORTB & 0xDF;
+  }
+
+}
