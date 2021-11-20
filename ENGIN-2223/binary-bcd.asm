41cb2300e7dfbbe0e3ec6488bb5f467c9672c0b3
DoctorPCOD <78432183+DoctorPCOD@users.noreply.github.com>
2021-11-19 19:52:24 -0600

    Add files via upload

---
+++ ENGIN-2223/binary-bcd.asm
@@ -0,0 +1,22 @@
+	; convert to BCD (hundreds place)
+	LDI r18, -1
+BCD1:
+	INC r18
+	SUBI r20, 100
+	BRCC BCD1
+	; (tens place)
+	LDI r21, 100
+	ADD r20, r21
+	LDI r17, -1
+BCD2:
+	INC r17
+	SUBI r20, 10
+	BRCC BCD2
+	; (ones place)
+	LDI r21, 10
+	ADD r20, r21
+	LDI r22, -1
+BCD3:
+	INC r22
+	SUBI r20, 1
+	BRCC BCD3
