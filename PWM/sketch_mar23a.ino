c4b4b4b312758fd2b04f786a40411dac94a249bc
DoctorPCOD <78432183+DoctorPCOD@users.noreply.github.com>
2022-03-23 13:38:49 -0500

    Add files via upload

---
+++ PWM/sketch_mar23a.ino
@@ -0,0 +1,15 @@
+void setup() {
+  cli();
+  DDRB = 0x04;
+  TCCR1A = 0x23;
+  TCCR1B = 0x1D;
+  OCR1A = 0xFFFF;
+  OCR1B = 32768;
+  sei();
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
