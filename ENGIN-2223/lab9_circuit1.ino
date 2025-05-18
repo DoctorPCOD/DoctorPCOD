4cde1d497fe09de7bb220c0db583df0c6a237e05
DoctorPCOD <78432183+DoctorPCOD@users.noreply.github.com>
2025-05-17 19:58:31 -0500

    Add files via upload

--- ENGIN-2223/lab9_circuit1.ino
+++ ENGIN-2223/lab9_circuit1.ino
@@ -4,31 +4,22 @@
     numerals 0-9 on a 7-segment common-cathode display
   By: Alyssa J. Pasquale, Ph.D.
   Written: October 1, 2017
-  Edited: April 7, 2025
+  Edited: May 10, 2025
   I/O Pins
-  A0:
-  A1:
-  A2:
-  A3:
-  A4:
-  A5:
-  D0:
-  D1:
-  D2:
-  D3:
-  D4:
-  D5:
-  D6:
-  D7:
-  D8:
-  D9:
   D10: RCLK (pin 12) on 74595
   D11: SER (pin 14) on 74595
-  D12:
   D13: SRCLK (pin 11) on 74595
 */
 
-void setup() {
+#define MCU __AVR_ATmega328P__
+#define F_CPU 16000000UL
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+volatile unsigned char x = 0;
+
+int main(void) {
   DDRB = 0x2C;   // SCK, MOSI, SS are output pins
   PORTB |= 0x04; // SS high
 
@@ -42,13 +33,12 @@ void setup() {
   TIMSK1 = 0x02;
   OCR1A = 31249;
   sei();
-}
 
-volatile unsigned char x = 0;
-
-void loop() {
   unsigned char numArray[10] = {0xFC, 0x60, 0xDA, 0xF2, 0x66, 0xB6, 0xBE, 0xE0, 0xFE, 0xF6};
-  writeToSPI(numArray[x]);
+
+  while (1) {
+    writeToSPI(numArray[x]);
+  }
 }
 
 void writeToSPI(unsigned char dataToWrite) {
